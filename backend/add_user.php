c446b861a8dcc7357a975a67f0dcbaae635981ba
Artur <90841963+artursuvor@users.noreply.github.com>
2025-01-22 10:48:59 +0100

    Start point

--- backend/add_user.php
+++ backend/add_user.php
@@ -16,9 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $username = $_POST['username'] ?? '';
     $email = $_POST['email'] ?? '';
     $password = $_POST['password'] ?? '';
-
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
     $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
     $stmt->bind_param("sss", $username, $email, $hashedPassword);
 
